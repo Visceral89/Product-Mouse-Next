b66c0dd1911458cdbe61e464e3c5795694d60383
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-18 16:24:06 +0100

    Changed name of activeSection

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from "react";
 import Navigation from "./components/Navigation";
 
 export default function Home() {
-	const [activeSection, setActiveSection] = useState(0);
+	const [activeSectionIndex, setActiveSectionIndex] = useState(0);
 	const section1Ref = useRef(null);
 	const section2Ref = useRef(null);
 	const section3Ref = useRef(null);
@@ -16,30 +16,26 @@ export default function Home() {
 		const observer = new IntersectionObserver(
 			(entries) => {
 				const visibleSection = entries.find((entry) => entry.isIntersecting);
-
 				if (visibleSection) {
-					const visibleSectionIndex = sectionRefs.findIndex(
-						(ref) => ref.current === visibleSection.target
-					);
-					setActiveSection(visibleSectionIndex);
+					const visibleSectionIndex = entries.indexOf(visibleSection);
+					setActiveSectionIndex(visibleSectionIndex);
 				}
 			},
-			{ threshold: 0.6 }
+			{ threshold: 0.5 }
 		);
 
+		// Observe each section
 		const refs = [section1Ref, section2Ref /* ... more refs */];
 		refs.forEach((ref) => {
 			if (ref.current) observer.observe(ref.current);
 		});
 
-		return () => {
-			observer.disconnect();
-		};
-	});
+		return () => observer.disconnect();
+	}, []);
 
 	return (
 		<main className={styles.main}>
-			<Navigation activeSelectionIndex={activeSection} />
+			<Navigation activeSelectionIndex={activeSectionIndex} />
 			{/* Replace theese divs with acctual components */}
 			<div id='section1'></div>
 			<div id='section2'></div>
