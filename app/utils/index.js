1538f25b77cec4a33adc7dfc914598a48e6fc072
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-19 08:01:06 +0100

    Barrel Export
    Trying this new method of exporting since i got this tip from Vercel.

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,1 +1,5 @@
-export { default as }
+export { default as HeroSection } from "../components/HeroSection";
+export { default as AboutSection } from "../components/AboutSection";
+export { default as DetailsSection } from "../components/DetailsSection";
+export { default as ProductSection } from "../components/ProductSection";
+export { default as PruductSection } from "../components/SpecsSection";
