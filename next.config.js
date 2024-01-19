6d622e1189e704f27d57660bf1ecd8c8d2834164
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-19 14:46:22 +0100

    Minor css fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: {
+		optimizePackageImports: ["utils"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
