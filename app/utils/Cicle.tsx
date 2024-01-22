298de81c06af9f9fe4ffa4c1789c6017026b342f
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-22 10:22:55 +0100

    Change size of circles

--- app/utils/Cicle.tsx
+++ app/utils/Cicle.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Circle() {
-	return <Image src='Circle.svg' height={8} width={8} alt='circle icon' />;
+	return <Image src='Circle.svg' height={12} width={12} alt='circle icon' />;
 }
--- app/utils/CicleActive.tsx
+++ app/utils/CicleActive.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function CircleActive() {
 	return (
-		<Image src='circle-active.svg' height={8} width={8} alt='circle icon' />
+		<Image src='circle-active.svg' height={12} width={12} alt='circle icon' />
 	);
 }
