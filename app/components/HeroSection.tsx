694e44764c3d8796434af77ce6e55f54fa138b8a
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-24 11:51:59 +0100

    Starting with the hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -14,8 +14,23 @@ const HeroSection = forwardRef<HTMLDivElement, Props>(
 		return (
 			<section id='section1' className={styles.Section} ref={ref}>
 				<div className={hero.sectionWrapper}>
-					<div>Hero Section</div>
-					<div>Image</div>
+					<div className={hero.informationWrapper}>
+						<div className={hero.textContainer}>
+							<h1>Forge your firepower</h1>
+							<p className={hero.textBold}>
+								all-new c404 for battle royale mastery. Focus your skill and own
+								every moment of the fight.
+							</p>
+							<p>
+								Make your weapons play harder in the configuration you deserver.
+								Use the programmable controls, LIGHTSPEED, Bluetooth wireless
+								16k dpi sensor and 240 battery life to the be the fastest
+								gunslinger on the battlefield.{" "}
+							</p>
+							<button>play advanced</button>
+						</div>
+					</div>
+					<div className={hero.imageWrapper}>Image</div>
 				</div>
 			</section>
 		);
