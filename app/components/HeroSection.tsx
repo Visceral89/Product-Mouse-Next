55c2f9be628babc53948e019159561174cfc1375
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-01-24 11:37:01 +0100

    Weird Hack
    Had to give Navigation container a width of 0 so it dosnt push any other elements. Even though zindex 999

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import styles from "../styles/Section.module.scss";
+import hero from "../styles/Hero.module.scss";
 import { forwardRef } from "react";
 import Navigation from "./Navigation";
+import Image from "next/image";
 
 interface Props {
 	activeSectionIndex: number;
@@ -11,7 +13,10 @@ const HeroSection = forwardRef<HTMLDivElement, Props>(
 	({ activeSectionIndex }, ref) => {
 		return (
 			<section id='section1' className={styles.Section} ref={ref}>
-				<div>Hero Section</div>
+				<div className={hero.sectionWrapper}>
+					<div>Hero Section</div>
+					<div>Image</div>
+				</div>
 			</section>
 		);
 	}
